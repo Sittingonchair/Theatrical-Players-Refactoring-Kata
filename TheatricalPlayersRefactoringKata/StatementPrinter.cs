fa7fe6fcbb4a274b6c85f1a630c17dddda6487d0
Miroslav Šerý <469343@muni.cz>
2023-03-07 19:08:28 +0100

    start refactoring into separate method

--- TheatricalPlayersRefactoringKata/StatementPrinter.cs
+++ TheatricalPlayersRefactoringKata/StatementPrinter.cs
@@ -7,6 +7,49 @@ namespace TheatricalPlayersRefactoringKata
 {
     public class StatementPrinter
     {
+        private string CalculateResult(Invoice invoice, Dictionary<string, Play> plays)
+        {
+            var totalAmount = 0;
+            var volumeCredits = 0;
+            StringBuilder result = new();
+
+            foreach (var perf in invoice.Performances)
+            {
+                var play = plays[perf.PlayID];
+                var perfAmount = 0;
+                switch (play.Type)
+                {
+                    case "tragedy":
+                        perfAmount = 40000;
+                        if (perf.Audience > 30)
+                        {
+                            perfAmount += 1000 * (perf.Audience - 30);
+                        }
+                        break;
+                    case "comedy":
+                        perfAmount = 30000;
+                        if (perf.Audience > 20)
+                        {
+                            perfAmount += 10000 + 500 * (perf.Audience - 20);
+                        }
+                        perfAmount += 300 * perf.Audience;
+                        break;
+                    default:
+                        throw new Exception("unknown type: " + play.Type);
+                }
+                // add volume credits
+                volumeCredits += Math.Max(perf.Audience - 30, 0);
+                // add extra credit for every ten comedy attendees
+                if ("comedy" == play.Type) volumeCredits += (int)Math.Floor((decimal)perf.Audience / 5);
+
+                // print line for this order
+                //result.AppendLine(string.Format(cultureInfo, "  {0}: {1:C} ({2} seats)", play.Name, Convert.ToDecimal(perfAmount / 100), perf.Audience));
+                totalAmount += perfAmount;
+            }
+
+            // todo: return a result
+        }
+
         public string Print(Invoice invoice, Dictionary<string, Play> plays)
         {
             var totalAmount = 0;
@@ -18,21 +61,21 @@ namespace TheatricalPlayersRefactoringKata
             foreach(var perf in invoice.Performances) 
             {
                 var play = plays[perf.PlayID];
-                var thisAmount = 0;
+                var perfAmount = 0;
                 switch (play.Type) 
                 {
                     case "tragedy":
-                        thisAmount = 40000;
+                        perfAmount = 40000;
                         if (perf.Audience > 30) {
-                            thisAmount += 1000 * (perf.Audience - 30);
+                            perfAmount += 1000 * (perf.Audience - 30);
                         }
                         break;
                     case "comedy":
-                        thisAmount = 30000;
+                        perfAmount = 30000;
                         if (perf.Audience > 20) {
-                            thisAmount += 10000 + 500 * (perf.Audience - 20);
+                            perfAmount += 10000 + 500 * (perf.Audience - 20);
                         }
-                        thisAmount += 300 * perf.Audience;
+                        perfAmount += 300 * perf.Audience;
                         break;
                     default:
                         throw new Exception("unknown type: " + play.Type);
@@ -43,8 +86,8 @@ namespace TheatricalPlayersRefactoringKata
                 if ("comedy" == play.Type) volumeCredits += (int)Math.Floor((decimal)perf.Audience / 5);
 
                 // print line for this order
-                result.AppendLine(string.Format(cultureInfo, "  {0}: {1:C} ({2} seats)", play.Name, Convert.ToDecimal(thisAmount / 100), perf.Audience));
-                totalAmount += thisAmount;
+                result.AppendLine(string.Format(cultureInfo, "  {0}: {1:C} ({2} seats)", play.Name, Convert.ToDecimal(perfAmount / 100), perf.Audience));
+                totalAmount += perfAmount;
             }
             result.AppendLine(string.Format(cultureInfo, "Amount owed is {0:C}", Convert.ToDecimal(totalAmount / 100)));
             result.AppendLine($"You earned {volumeCredits} credits");
